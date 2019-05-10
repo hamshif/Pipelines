d5bccce546452a25df7c9a760fcad0eda564f16d
Gideon Bar <gbar@marketo.com>
2019-05-10 06:01:41 +0300

    snippet cleanup

--- Snippets/src/main/scala/com/hamshif/wielder/pipelines/snippets/AggregateKeepingRowJob.scala
+++ Snippets/src/main/scala/com/hamshif/wielder/pipelines/snippets/AggregateKeepingRowJob.scala
@@ -1,59 +1,55 @@
-package com.hamshif.wielder.pipelines.snippets
+    package com.hamshif.wielder.pipelines.snippets
 
-import org.apache.spark.sql._
-import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.StringType
-
-import scala.collection.mutable
+    import org.apache.spark.sql._
+    import org.apache.spark.sql.functions._
 
 
-object AggregateKeepingRowJob {
+    object AggregateKeepingRowJob {
 
-  def main (args: Array[String]): Unit = {
+      def main (args: Array[String]): Unit = {
 
-    val sparkSession = SparkSession
-      .builder()
-      .appName(this.getClass.getName.replace("$", ""))
-      .master("local")
-      .getOrCreate()
+        val sparkSession = SparkSession
+          .builder()
+          .appName(this.getClass.getName.replace("$", ""))
+          .master("local")
+          .getOrCreate()
 
-    val sc = sparkSession.sparkContext
-    sc.setLogLevel("ERROR")
+        val sc = sparkSession.sparkContext
+        sc.setLogLevel("ERROR")
 
-    import sparkSession.sqlContext.implicits._
+        import sparkSession.sqlContext.implicits._
 
-    val rawDf = Seq(
-      (1L, "Moe",  "Slap",  2.0, 18),
-      (2L, "Larry",  "Spank",  3.0, 15),
-      (3L, "Curly",  "Twist", 5.0, 15),
-      (4L, "Laurel", "Whimper", 3.0, 15),
-      (5L, "Hardy", "Laugh", 6.0, 15),
-      (6L, "Charley",  "Ignore",   5.0, 5)
-    ).toDF("id", "name", "requisite", "money", "age")
+        val rawDf = Seq(
+          (1L, "Moe",  "Slap",  2.0, 18),
+          (2L, "Larry",  "Spank",  3.0, 15),
+          (3L, "Curly",  "Twist", 5.0, 15),
+          (4L, "Laurel", "Whimper", 3.0, 15),
+          (5L, "Hardy", "Laugh", 6.0, 15),
+          (6L, "Charley",  "Ignore",   5.0, 5)
+        ).toDF("id", "name", "requisite", "money", "age")
 
-    rawDf.show(false)
-    rawDf.printSchema
+        rawDf.show(false)
+        rawDf.printSchema
 
-    val maxAgeUdaf = new KeepRowWithMaxAge
+        val maxAgeUdaf = new KeepRowWithMaxAge
 
-    val aggDf = rawDf
-      .groupBy("age")
-      .agg(
-        count("id"),
-        max(col("money")),
-        maxAgeUdaf(
-          col("id"),
-          col("name"),
-          col("requisite"),
-          col("money"),
-          col("age")).as("KeepRowWithMaxAge")
-      )
+        val aggDf = rawDf
+          .groupBy("age")
+          .agg(
+            count("id"),
+            max(col("money")),
+            maxAgeUdaf(
+              col("id"),
+              col("name"),
+              col("requisite"),
+              col("money"),
+              col("age")).as("KeepRowWithMaxAge")
+          )
 
-    aggDf.printSchema
-    aggDf.show(false)
+        aggDf.printSchema
+        aggDf.show(false)
 
-  }
+      }
 
 
-}
+    }
--- Snippets/src/main/scala/com/hamshif/wielder/pipelines/snippets/KeepRowWithMaxAmt.scala
+++ Snippets/src/main/scala/com/hamshif/wielder/pipelines/snippets/KeepRowWithMaxAmt.scala
@@ -1,75 +1,75 @@
-  package com.hamshif.wielder.pipelines.snippets
+    package com.hamshif.wielder.pipelines.snippets
 
-  import org.apache.spark.sql.Row
-  import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
-  import org.apache.spark.sql.types._
+    import org.apache.spark.sql.Row
+    import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
+    import org.apache.spark.sql.types._
 
 
-  class KeepRowWithMaxAmt extends UserDefinedAggregateFunction {
-    // This is the input fields for your aggregate function.
-    override def inputSchema: org.apache.spark.sql.types.StructType =
-      StructType(
+    class KeepRowWithMaxAmt extends UserDefinedAggregateFunction {
+      // This is the input fields for your aggregate function.
+      override def inputSchema: org.apache.spark.sql.types.StructType =
+        StructType(
+          StructField("store", StringType) ::
+          StructField("prod", StringType) ::
+          StructField("amt", DoubleType) ::
+          StructField("units", IntegerType) :: Nil
+        )
+
+      // This is the internal fields you keep for computing your aggregate.
+      override def bufferSchema: StructType = StructType(
         StructField("store", StringType) ::
         StructField("prod", StringType) ::
         StructField("amt", DoubleType) ::
         StructField("units", IntegerType) :: Nil
       )
 
-    // This is the internal fields you keep for computing your aggregate.
-    override def bufferSchema: StructType = StructType(
-      StructField("store", StringType) ::
-      StructField("prod", StringType) ::
-      StructField("amt", DoubleType) ::
-      StructField("units", IntegerType) :: Nil
-    )
 
+      // This is the output type of your aggregation function.
+      override def dataType: DataType =
+        StructType((Array(
+          StructField("store", StringType),
+          StructField("prod", StringType),
+          StructField("amt", DoubleType),
+          StructField("units", IntegerType)
+        )))
 
-    // This is the output type of your aggregation function.
-    override def dataType: DataType =
-      StructType((Array(
-        StructField("store", StringType),
-        StructField("prod", StringType),
-        StructField("amt", DoubleType),
-        StructField("units", IntegerType)
-      )))
+      override def deterministic: Boolean = true
 
-    override def deterministic: Boolean = true
+      // This is the initial value for your buffer schema.
+      override def initialize(buffer: MutableAggregationBuffer): Unit = {
+        buffer(0) = ""
+        buffer(1) = ""
+        buffer(2) = 0.0
+        buffer(3) = 0
+      }
 
-    // This is the initial value for your buffer schema.
-    override def initialize(buffer: MutableAggregationBuffer): Unit = {
-      buffer(0) = ""
-      buffer(1) = ""
-      buffer(2) = 0.0
-      buffer(3) = 0
-    }
+      // This is how to update your buffer schema given an input.
+      override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
 
-    // This is how to update your buffer schema given an input.
-    override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
+        val amt = buffer.getAs[Double](2)
+        val candidateAmt = input.getAs[Double](2)
 
-      val amt = buffer.getAs[Double](2)
-      val candidateAmt = input.getAs[Double](2)
+        amt match {
+          case a if a < candidateAmt =>
+            buffer(0) = input.getAs[String](0)
+            buffer(1) = input.getAs[String](1)
+            buffer(2) = input.getAs[Double](2)
+            buffer(3) = input.getAs[Int](3)
+          case _ =>
+        }
+      }
 
-      amt match {
-        case a if a < candidateAmt =>
-          buffer(0) = input.getAs[String](0)
-          buffer(1) = input.getAs[String](1)
-          buffer(2) = input.getAs[Double](2)
-          buffer(3) = input.getAs[Int](3)
-        case _ =>
+      // This is how to merge two objects with the bufferSchema type.
+      override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
+
+        buffer1(0) = buffer2.getAs[String](0)
+        buffer1(1) = buffer2.getAs[String](1)
+        buffer1(2) = buffer2.getAs[Double](2)
+        buffer1(3) = buffer2.getAs[Int](3)
+      }
+
+      // This is where you output the final value, given the final value of your bufferSchema.
+      override def evaluate(buffer: Row): Any = {
+        buffer
       }
     }
-
-    // This is how to merge two objects with the bufferSchema type.
-    override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
-
-      buffer1(0) = buffer2.getAs[String](0)
-      buffer1(1) = buffer2.getAs[String](1)
-      buffer1(2) = buffer2.getAs[Double](2)
-      buffer1(3) = buffer2.getAs[Int](3)
-    }
-
-    // This is where you output the final value, given the final value of your bufferSchema.
-    override def evaluate(buffer: Row): Any = {
-      buffer
-    }
-  }
