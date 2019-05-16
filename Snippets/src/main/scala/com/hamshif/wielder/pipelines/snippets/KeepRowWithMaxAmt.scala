40373a0d3ee32104900ce2f00c65a83333e07714
Gideon Bar <gbar@marketo.com>
2019-05-16 08:18:19 +0300

    indentation

--- Snippets/src/main/scala/com/hamshif/wielder/pipelines/snippets/KeepRowWithMaxAmt.scala
+++ Snippets/src/main/scala/com/hamshif/wielder/pipelines/snippets/KeepRowWithMaxAmt.scala
@@ -1,75 +1,75 @@
-    package com.hamshif.wielder.pipelines.snippets
+package com.hamshif.wielder.pipelines.snippets
 
-    import org.apache.spark.sql.Row
-    import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
-    import org.apache.spark.sql.types._
-
-
-    class KeepRowWithMaxAmt extends UserDefinedAggregateFunction {
-      // This is the input fields for your aggregate function.
-      override def inputSchema: org.apache.spark.sql.types.StructType =
-        StructType(
-          StructField("store", StringType) ::
-          StructField("prod", StringType) ::
-          StructField("amt", DoubleType) ::
-          StructField("units", IntegerType) :: Nil
-        )
-
-      // This is the internal fields you keep for computing your aggregate.
-      override def bufferSchema: StructType = StructType(
-        StructField("store", StringType) ::
-        StructField("prod", StringType) ::
-        StructField("amt", DoubleType) ::
-        StructField("units", IntegerType) :: Nil
-      )
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
+import org.apache.spark.sql.types._
 
 
-      // This is the output type of your aggregation function.
-      override def dataType: DataType =
-        StructType((Array(
-          StructField("store", StringType),
-          StructField("prod", StringType),
-          StructField("amt", DoubleType),
-          StructField("units", IntegerType)
-        )))
+class KeepRowWithMaxAmt extends UserDefinedAggregateFunction {
+  // This is the input fields for your aggregate function.
+  override def inputSchema: org.apache.spark.sql.types.StructType =
+    StructType(
+      StructField("store", StringType) ::
+      StructField("prod", StringType) ::
+      StructField("amt", DoubleType) ::
+      StructField("units", IntegerType) :: Nil
+    )
 
-      override def deterministic: Boolean = true
+  // This is the internal fields you keep for computing your aggregate.
+  override def bufferSchema: StructType = StructType(
+    StructField("store", StringType) ::
+    StructField("prod", StringType) ::
+    StructField("amt", DoubleType) ::
+    StructField("units", IntegerType) :: Nil
+  )
 
-      // This is the initial value for your buffer schema.
-      override def initialize(buffer: MutableAggregationBuffer): Unit = {
-        buffer(0) = ""
-        buffer(1) = ""
-        buffer(2) = 0.0
-        buffer(3) = 0
-      }
 
-      // This is how to update your buffer schema given an input.
-      override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
+  // This is the output type of your aggregation function.
+  override def dataType: DataType =
+    StructType((Array(
+      StructField("store", StringType),
+      StructField("prod", StringType),
+      StructField("amt", DoubleType),
+      StructField("units", IntegerType)
+    )))
 
-        val amt = buffer.getAs[Double](2)
-        val candidateAmt = input.getAs[Double](2)
+  override def deterministic: Boolean = true
 
-        amt match {
-          case a if a < candidateAmt =>
-            buffer(0) = input.getAs[String](0)
-            buffer(1) = input.getAs[String](1)
-            buffer(2) = input.getAs[Double](2)
-            buffer(3) = input.getAs[Int](3)
-          case _ =>
-        }
-      }
+  // This is the initial value for your buffer schema.
+  override def initialize(buffer: MutableAggregationBuffer): Unit = {
+    buffer(0) = ""
+    buffer(1) = ""
+    buffer(2) = 0.0
+    buffer(3) = 0
+  }
 
-      // This is how to merge two objects with the bufferSchema type.
-      override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
+  // This is how to update your buffer schema given an input.
+  override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
 
-        buffer1(0) = buffer2.getAs[String](0)
-        buffer1(1) = buffer2.getAs[String](1)
-        buffer1(2) = buffer2.getAs[Double](2)
-        buffer1(3) = buffer2.getAs[Int](3)
-      }
+    val amt = buffer.getAs[Double](2)
+    val candidateAmt = input.getAs[Double](2)
 
-      // This is where you output the final value, given the final value of your bufferSchema.
-      override def evaluate(buffer: Row): Any = {
-        buffer
-      }
+    amt match {
+      case a if a < candidateAmt =>
+        buffer(0) = input.getAs[String](0)
+        buffer(1) = input.getAs[String](1)
+        buffer(2) = input.getAs[Double](2)
+        buffer(3) = input.getAs[Int](3)
+      case _ =>
     }
+  }
+
+  // This is how to merge two objects with the bufferSchema type.
+  override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
+
+    buffer1(0) = buffer2.getAs[String](0)
+    buffer1(1) = buffer2.getAs[String](1)
+    buffer1(2) = buffer2.getAs[Double](2)
+    buffer1(3) = buffer2.getAs[Int](3)
+  }
+
+  // This is where you output the final value, given the final value of your bufferSchema.
+  override def evaluate(buffer: Row): Any = {
+    buffer
+  }
+}
--- Snippets/src/main/scala/com/hamshif/wielder/pipelines/snippets/ReduceAggJob.scala
+++ Snippets/src/main/scala/com/hamshif/wielder/pipelines/snippets/ReduceAggJob.scala
@@ -1,58 +1,56 @@
-        package com.hamshif.wielder.pipelines.snippets
+package com.hamshif.wielder.pipelines.snippets
 
-    import org.apache.spark.sql._
-    import org.apache.spark.sql.functions._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
 
 
-    object ReduceAggJob {
+object ReduceAggJob {
 
-      def main (args: Array[String]): Unit = {
+  def main (args: Array[String]): Unit = {
 
-        val appName = this.getClass.getName.replace("$", "")
-        println(s"appName: $appName")
+    val appName = this.getClass.getName.replace("$", "")
+    println(s"appName: $appName")
 
-        val sparkSession = SparkSession
-          .builder()
-          .appName(appName)
-          .master("local")
-          .getOrCreate()
+    val sparkSession = SparkSession
+      .builder()
+      .appName(appName)
+      .master("local")
+      .getOrCreate()
 
-        val sc = sparkSession.sparkContext
-        sc.setLogLevel("ERROR")
+    val sc = sparkSession.sparkContext
+    sc.setLogLevel("ERROR")
 
-        import sparkSession.sqlContext.implicits._
+    import sparkSession.sqlContext.implicits._
 
-        val rawDf = Seq(
-          ("West",  "Apple",  2.0, 10),
-          ("West",  "Apple",  3.0, 15),
-          ("West",  "Orange", 5.0, 15),
-          ("West",  "Orange", 17.0, 15),
-          ("South", "Orange", 3.0, 9),
-          ("South", "Orange", 6.0, 18),
-          ("East",  "Milk",   5.0, 5)
-        ).toDF("store", "prod", "amt", "units")
+    val rawDf = Seq(
+      ("West",  "Apple",  2.0, 10),
+      ("West",  "Apple",  3.0, 15),
+      ("West",  "Orange", 5.0, 15),
+      ("West",  "Orange", 17.0, 15),
+      ("South", "Orange", 3.0, 9),
+      ("South", "Orange", 6.0, 18),
+      ("East",  "Milk",   5.0, 5)
+    ).toDF("store", "prod", "amt", "units")
 
-        sparkSession.udf.register("gm", new GeometricMean)
+    rawDf.printSchema
+    rawDf.show(false)
 
-        rawDf.printSchema
-        rawDf.show(false)
+    val maxAmtUdaf = new KeepRowWithMaxAmt
 
-        val maxAmtUdaf = new KeepRowWithMaxAmt
+    // Keep the row with max amt
+    val aggDf = rawDf
+      .groupBy("store", "prod")
+      .agg(
+        max("amt"),
+        avg("units"),
+        maxAmtUdaf(
+        col("store"),
+        col("prod"),
+        col("amt"),
+        col("units")).as("KeepRowWithMaxAmt")
+      )
 
-        // Keep the row with max amt
-        val aggDf = rawDf
-          .groupBy("store", "prod")
-          .agg(
-            max("amt"),
-            avg("units"),
-            maxAmtUdaf(
-            col("store"),
-            col("prod"),
-            col("amt"),
-            col("units")).as("KeepRowWithMaxAmt")
-          )
-
-        aggDf.printSchema
-        aggDf.show(false)
-      }
-    }
+    aggDf.printSchema
+    aggDf.show(false)
+  }
+}
